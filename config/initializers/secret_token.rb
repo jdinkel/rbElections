14184be9a4475957b71f1c797fa6201f21fed839
James Dinkel <jdinkel@gmail.com>
2011-04-29 08:59:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RbElections::Application.config.secret_token = 'fe7291e7e212022d2f96f1c64f0af1a2ffbc58c55794da793e0a8ea13bcf2d9081f80528ce08e6c300b0af880ccffe2592545a4f508b8961fa8239d4017bab9d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RbElections::Application.config.session_store :cookie_store, :key => '_rbElections_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RbElections::Application.config.session_store :active_record_store
