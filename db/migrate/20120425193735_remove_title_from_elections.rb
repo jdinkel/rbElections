d3f0647c9126cbca4a88e1b1fd65e9ef9925745d
James Dinkel <jdinkel@gmail.com>
2012-04-26 11:42:48 -0500

    Tweaked migration to allow migrating down

--- db/migrate/20120425193735_remove_title_from_elections.rb
+++ db/migrate/20120425193735_remove_title_from_elections.rb
@@ -1,6 +1,17 @@
 class RemoveTitleFromElections < ActiveRecord::Migration
-  def change
+  def up
     remove_column :elections, :title
     add_index :elections, :date, :unique => true
+  end
+
+  def down
+    add_column :elections, :title, :string
+    Election.all.each do |e|
+      e.title = "#{e.type} Election On #{e.date}"
+      e.save!
+    end
+    change_column :elections, :title, :string, :null => false
+    remove_index :elections, :date
+    add_index :elections, :title, :unique => true
   end
 end
